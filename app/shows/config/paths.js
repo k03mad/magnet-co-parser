9f27a28197d32b0d7bfb702d6782789e026403eb
k03mad <k03.mad@gmail.com>
2019-10-09 15:40:56 +0300

    debuggen

--- app/shows/config/paths.js
+++ app/shows/config/paths.js
@@ -2,6 +2,7 @@ import appRoot from 'app-root-path';
 
 export default {
     templates: {
+        folder: `${appRoot}/templates/`,
         list: `${appRoot}/templates/list.html`,
         page: `${appRoot}/templates/page.html`,
     },
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import filmsGenerator from './app/films/generator.js';
 import filmsParse from './app/films/parse.js';
+import fs from 'fs';
 import pathsFilms from './app/films/config/paths.js';
 import pathsShows from './app/shows/config/paths.js';
 import showsGenerator from './app/shows/generator.js';
@@ -8,12 +9,39 @@ import utils from 'utils-mad';
 
 (async () => {
     try {
-        const [proxy] = await utils.request.proxy();
+        let filmsData, showsData;
 
-        const [showsData, filmsData] = await Promise.all([
-            showsParse(proxy),
-            filmsParse(proxy),
-        ]);
+        // только генерим страницы из json-файлов для отладки
+        if (process.env.npm_config_onlygen) {
+
+            [filmsData, showsData] = await Promise.all([
+                fs.promises.readFile(`${pathsShows.templates.folder}films.json`, 'utf8'),
+                fs.promises.readFile(`${pathsShows.templates.folder}shows.json`, 'utf8'),
+            ]);
+
+            filmsData = JSON.parse(filmsData.replace(/[\n ]/g, ''));
+            showsData = JSON.parse(showsData.replace(/[\n ]/g, ''));
+
+        // иначе парсим
+        } else {
+
+            const [proxy] = await utils.request.proxy();
+
+            [showsData, filmsData] = await Promise.all([
+                showsParse(proxy),
+                filmsParse(proxy),
+            ]);
+
+            // сохраняем json-файлы для отладки
+            if (process.env.npm_config_onlysave) {
+
+                await Promise.all([
+                    fs.promises.writeFile(`${pathsShows.templates.folder}films.json`, JSON.stringify(filmsData, null, 4)),
+                    fs.promises.writeFile(`${pathsShows.templates.folder}shows.json`, JSON.stringify(showsData, null, 4)),
+                ]);
+
+            }
+        }
 
         await utils.folder.erase([
             pathsFilms.www.folder,
@@ -25,6 +53,7 @@ import utils from 'utils-mad';
             showsGenerator(showsData),
             filmsGenerator(filmsData),
         ]);
+
     } catch (err) {
         utils.print.ex(err, {exit: true, full: true});
     }
