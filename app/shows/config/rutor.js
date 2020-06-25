9bfdde6022fd442c8ebeb88eab0d7b777529ae23
Kirill Molchanov <k03.mad@gmail.com>
2020-06-25 08:38:17 +0300

    info

--- app/shows/config/rutor.js
+++ app/shows/config/rutor.js
@@ -1,7 +1,7 @@
 export default {
     search: {
         // rutor.info/search/страница/категория/все слова/сортировка по сидам/
-        url: 'http://rutor.is/search/0/0/100/2/',
+        url: 'http://rutor.info/search/0/0/100/2/',
         quality: {
             full: ' 1080p',
             back: ' 720p',
