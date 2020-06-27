bd476223b9c77af6fb1dd2112afc6b1e577a583a
k03mad <k03.mad@gmail.com>
2020-06-27 17:37:20 +0300

    fix parse

--- index.js
+++ index.js
@@ -25,9 +25,6 @@ const parsers = [
 (async () => {
     try {
         await utils.folder.erase([
-            pathsFilms.parsed.folder,
-            pathsShows.parsed.folder,
-
             pathsFilms.www.folder,
             pathsShows.www.folder,
 
@@ -35,19 +32,29 @@ const parsers = [
             pathsShows.www.pages,
         ]);
 
+        let error;
+
         await Promise.all(parsers.map(async elem => {
+            try {
 
-            const data = await elem.parser();
+                const data = await elem.parser();
 
-            await fs.promises.writeFile(
-                `${elem.folder + elem.name}.json`,
-                JSON.stringify(data, null, 4),
-            );
+                await fs.promises.writeFile(
+                    `${elem.folder + elem.name}.json`,
+                    JSON.stringify(data, null, 4),
+                );
 
-            await elem.generator(data);
+                await elem.generator(data);
 
+            } catch (err) {
+                error = err;
+            }
         }));
+
+        if (error) {
+            utils.print.ex(error, {exit: true});
+        }
     } catch (err) {
-        utils.print.ex(err, {full: true, exit: true});
+        utils.print.ex(err, {exit: true});
     }
 })();
