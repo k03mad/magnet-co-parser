59368d132defd3347dbc8271e0fd7b70e540de67
k03mad <k03.mad@gmail.com>
2021-07-19 21:57:40 +0300

    decrease covers

--- app/films/config/rutor.js
+++ app/films/config/rutor.js
@@ -34,7 +34,7 @@ export default {
             ].join(' '),
         },
         quality: ' 1080p',
-        pages: 20,
+        pages: 3,
         categories: [1, 5, 6, 7, 10, 12],
     },
     selectors: {
