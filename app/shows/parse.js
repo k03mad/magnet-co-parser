d2c93b1718ffac372b71d7d3144b0ff27a12a605
k03mad <k03.mad@gmail.com>
2022-05-01 16:10:50 +0300

    return tmdb url

--- app/shows/parse.js
+++ app/shows/parse.js
@@ -220,9 +220,10 @@ export default async proxy => {
                     .filter(elem => Boolean(elem.profile_path))
                     .map(elem => ({
                         id: elem.id,
-                        link: elem.imdb_id
-                            ? config.service.imdb.person + elem.imdb_id
-                            : config.service.tmdb.person + elem.id,
+                        link:
+                            // elem.imdb_id
+                            // ? config.service.imdb.person + elem.imdb_id
+                            config.service.tmdb.person + elem.id,
                         name: elem.name,
                         cover: config.service.tmdb.cover + elem.profile_path,
                     })),
